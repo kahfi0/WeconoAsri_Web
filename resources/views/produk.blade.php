67b547dd6caa4d329bfcc0a6e11d416c8bc7002e
kahfi0 <eon8853@gmail.com>
2022-06-22 04:58:05 +0700

    addprodukfix

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -69,17 +69,70 @@
 
         </div><!-- /.container-fluid -->
 
-    <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
 
-    </div> --> -->
+              <form method="POST" action="{{ route('produk.store') }}" role="form" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                  <div class="form-group">
+                    <label>Nama</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nama" name="name">
+                  </div>
+                  <div class="row">
+                    <div class="col-sm-6">
+                      <!-- text input -->
+                      <div class="form-group">
+                        <label>Harga</label>
+                        <input type="text" class="form-control" placeholder="Harga ..." name="harga">
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Pilih Category</label>
+                        <select class="form-control" name="category_id">
+                          <option value="1">Batik Tulis</option>
+                          <!-- <option value="1">option 2</option>
+                          <option value="1">option 3</option>
+                          <option value="1">option 4</option>
+                          <option value="1">option 5</option> -->
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <label>Deskripsi</label>
+                    <textarea class="form-control" rows="3" placeholder="Deskripsi ..." name="deskripsi"></textarea>
+                  </div>
+
+                  <div class="form-group">
+                    <label for="exampleInputFile">File Gambar</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile" name="image">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-primary">Tambah</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
 
     <div class="modal modal-danger fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
